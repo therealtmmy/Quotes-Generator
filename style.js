27da019bf170a523b12b9424ef83288b6beb7fd3
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-08-05 21:34:35 +0100

    Adjusted the background

--- style.js
+++ style.js
@@ -1,41 +1,28 @@
 // Here you find me playing with styling using JavaScript
 
-document.body.style.backgroundImage = "url('abstract-purple-watercolor-background-illustration-high-resolution-free-photo.jpg')"
+document.querySelector(".Center-div").style.display = "flex";
+document.querySelector(".Center-div").style.flexDirection = "column";
+document.querySelector(".Center-div").style.alignItems = "center";
+document.querySelector(".Center-div").style.justifyContent = "center";
+document.querySelector(".Center-div").style.height = "100vh";
 
+document.querySelector(".Quote-Generator").style.display = "flex";
+document.querySelector(".Quote-Generator").style.alignItems = "center";
+document.querySelector(".Quote-Generator").style.justifyContent = "center";
+document.querySelector(".Quote-Generator").style.textAlign = "center";
 
-document.querySelector(".Center-div").style.display = 'flex'
-document.querySelector(".Center-div").style.flexDirection = 'column'
-document.querySelector(".Center-div").style.alignItems = 'center'
-document.querySelector(".Center-div").style.justifyContent = 'center'
-document.querySelector(".Center-div").style.height = '100vh'
+document.querySelector("#Quotation").style.fontSize = "10em";
+document.querySelector("#Quotation").style.marginBottom = "-150px";
+document.querySelector("#Quotation").style.zIndex = "1";
+document.querySelector("#Quotation").style.color = "black";
+document.querySelector("#Quotation").style.fontFamily =
+  "Verdana, Geneva, Tahoma, sans-serif";
 
-
-
-document.querySelector(".Quote-Generator").style.display = 'flex'
-document.querySelector(".Quote-Generator").style.alignItems = 'center'
-document.querySelector(".Quote-Generator").style.justifyContent = 'center'
-document.querySelector(".Quote-Generator").style.textAlign = 'center'
-
-
-
-document.querySelector("#Quotation").style.fontSize = '10em'
-document.querySelector("#Quotation").style.marginBottom = '-150px'
-document.querySelector("#Quotation").style.zIndex = '1'
-document.querySelector("#Quotation").style.color = 'black'
-document.querySelector("#Quotation").style.fontFamily = 'Verdana, Geneva, Tahoma, sans-serif'
-
-
-
-document.querySelector(".Quote-button").style.width = '120px'
-document.querySelector(".Quote-button").style.height = '35px'
-document.querySelector(".Quote-button").style.backgroundColor = 'black'
-document.querySelector(".Quote-button").style.color = 'white'
-document.querySelector(".Quote-button").style.fontSize = '15px'
-document.querySelector(".Quote-button").style.marginTop = '20px'
-document.querySelector(".Quote-button").style.borderRadius = '15px'
-document.querySelector(".Quote-button").style.cursor = 'pointer'
-
-
-
-
-
+document.querySelector(".Quote-button").style.width = "120px";
+document.querySelector(".Quote-button").style.height = "35px";
+document.querySelector(".Quote-button").style.backgroundColor = "black";
+document.querySelector(".Quote-button").style.color = "white";
+document.querySelector(".Quote-button").style.fontSize = "15px";
+document.querySelector(".Quote-button").style.marginTop = "20px";
+document.querySelector(".Quote-button").style.borderRadius = "15px";
+document.querySelector(".Quote-button").style.cursor = "pointer";
